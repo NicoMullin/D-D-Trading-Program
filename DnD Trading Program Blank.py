1650bf057346e852cbb496d933bc0f5f838204f0
NicoMullin <timdarknesslord@gmail.com>
2025-01-13 19:31:17 -0600

    Update DnD Trading Program Blank.py
    Added lines
    
    # Copyright (c) 2025 Nico Mullin
    # Licensed under the MIT License. See LICENSE file for details.

--- DnD Trading Program Blank.py
+++ DnD Trading Program Blank.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Nico Mullin
+# Licensed under the MIT License. See LICENSE file for details.
+
 import random
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
